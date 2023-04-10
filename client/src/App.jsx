005d60a08bc7f1e52be5a26c808cd014167bdb12
Rafael Brito <rushbig@hotmail.com>
2023-04-10 15:34:11 -0300

    adicionando ex 04

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Card } from './components/Card';
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar'
 
@@ -6,8 +7,23 @@ import { Navbar } from './components/Navbar'
 function App() {
   
   return (
-    <div>
+    <div className='app'>
       <Navbar/>
+
+          <main className='main'>
+
+           <h2>Produtos</h2> 
+           <Card 
+                imagem='Http://localhost:8081/assets/brandade.png'
+                nome='Brandade de Bacalhau'
+                descricao='Aveludada brandade de bacalhau servida em crocante de sementes, delicada base de azeite de rúcula e suas folhas com farofinha de azeitonas.'
+                valor= {65}
+                tempoPreparo='30 min'
+                
+              />
+
+          </main> 
+
       <Footer/>
     </div>
       
